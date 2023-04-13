170c72a20d64ecd8a1db597e4c2c464198771317
tdregmans <tdregmans+github@protonmail.com>
2023-04-13 15:33:49 +0200

    opdracht1.1

--- opdracht1/opdracht1.hs
+++ opdracht1/opdracht1.hs
@@ -1,17 +1,19 @@
 {--
-    opdracht1.hs
+    sortfile.hs
     TINPRO01-8 (Functional programming 2)
     Sep van der Biezen, Thijs Dregmans 
     Last edited: 2023-04-13
 --}
 
 module Main where
-    import Data.Char
-    
-    import System.Environment
 
-    main = do
-        text <- readFile "test.txt"
-        let processed = reverse $map toUpper text
-        writeFile "test2.txt" processed
-        putStrLn "file written"
+import Data.List
+
+import System.Environment
+
+main = do [sourcefile, targetfile] <- getArgs
+          filecontent <- readFile sourcefile
+          print filecontent
+          let sortedFilecontent = sort filecontent
+          print sortedFilecontent
+          writeFile targetfile sortedFilecontent
