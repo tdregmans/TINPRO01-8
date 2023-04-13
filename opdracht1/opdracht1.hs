0add38c01682e0fb057e964c2cc2de381b4c8bfe
tdregmans <tdregmans+github@protonmail.com>
2023-04-13 15:13:16 +0200

    delete opdracht1

--- opdracht1/opdracht1.hs
+++ opdracht1/opdracht1.hs
@@ -1,1 +1,17 @@
+{--
+    opdracht1.hs
+    TINPRO01-8 (Functional programming 2)
+    Sep van der Biezen, Thijs Dregmans 
+    Last edited: 2023-04-12
+--}
+
 module Main where
+    import Data.Char
+    
+    import System.Environment
+
+    main = do
+        text <- readFile "test.txt"
+        let processed = reverse $map toUpper text
+        writeFile "test2.txt" processed
+        putStrLn "file written"
