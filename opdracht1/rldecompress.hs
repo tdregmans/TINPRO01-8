028c4b36bf7803a8a635b18a6e14805b1a31a446
Andevai126 <sepvanderbiezen@gmail.com>
2023-04-13 21:50:36 +0200

    Update rldecompress.hs

--- opdracht1/rldecompress.hs
+++ opdracht1/rldecompress.hs
@@ -9,39 +9,22 @@ module Main where
 
 import Data.List
 import Data.Char
-
 import System.Environment
+import Text.Read
 
--- check :: String -> String
--- check x
---     | length x > 1 = show (length x) ++ nub x
---     | otherwise = x
-
-check :: String -> Bool
-check (x:xs:xss)
-    | isDigit x && isDigit xs = 
-    | isDigit x = 
-    | otherwise = x
-
-specialGroup x = reverse (splitPlaces)
-
-decompress :: String -> String
-decompress input = concat (map check (group input))
-
-
+decompress :: [Char] -> [Char]
+decompress x
+  | null x = []
+  | length x == 1 = x
+  | otherwise = replicate n (x !! index) ++ decompress (drop (index+1) x)
+    where index = length (takeWhile isDigit x)
+          strNum = take index x
+          n = if strNum == "" then 1 else read strNum :: Int
 
 main = do [sourcefile, targetfile] <- getArgs
           filecontent <- readFile sourcefile
 
           let uncompressedContent = decompress filecontent
-          let lenSource = length filecontent
-          let lenDecompressed = length uncompressedContent
-          let factor = round ((fromIntegral lenDecompressed) / (fromIntegral lenSource) * 100)
-          
-          
-          putStrLn $ "length of " ++ sourcefile ++ ": " ++ show lenSource ++ " characters"
-          putStrLn $ "length of decompressed file " ++ targetfile ++ ": " ++ show lenDecompressed ++ " characters"
-          putStrLn $ "factor " ++ show lenDecompressed ++ "/" ++ show lenSource ++ "*100=" ++ show factor ++ "%"
-          
+
           writeFile targetfile uncompressedContent
           putStrLn "done..."
