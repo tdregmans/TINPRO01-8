ace29e21a66cc7b01950878af393f2b40871b61d
tdregmans <tdregmans+github@protonmail.com>
2023-04-13 15:20:21 +0200

    commit

--- opdracht1/opdracht1.hs
+++ opdracht1/opdracht1.hs
@@ -2,7 +2,7 @@
     opdracht1.hs
     TINPRO01-8 (Functional programming 2)
     Sep van der Biezen, Thijs Dregmans 
-    Last edited: 2023-04-12
+    Last edited: 2023-04-13
 --}
 
 module Main where
