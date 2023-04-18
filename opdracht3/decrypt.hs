b8db25f1f1d2b8c270b4694ceb865f3fc18e11bf
tdregmans <tdregmans+github@protonmail.com>
2023-04-18 09:19:52 +0200

    voorbereiding opdracht3

--- opdracht3/decrypt.hs
+++ opdracht3/decrypt.hs
@@ -4,3 +4,26 @@
     Sep van der Biezen, Thijs Dregmans 
     Last edited: 2023-04-18
 --}
+
+module Main where
+
+import System.IO
+import System.Random
+import Data.Bits
+
+-- Function for reading encypted file
+
+
+
+-- Function for reading key
+
+
+
+-- Function for decrypting file with key
+
+
+
+-- Function for writing result in file
+
+
+
--- opdracht3/encrypt.hs
+++ opdracht3/encrypt.hs
@@ -4,3 +4,27 @@
     Sep van der Biezen, Thijs Dregmans 
     Last edited: 2023-04-18
 --}
+
+module Main where
+
+import System.IO
+import System.Random
+import Data.Bits
+
+-- Function for reading file
+
+
+
+-- Function for generating key
+
+
+
+-- Function for encrypting file with key
+
+
+
+-- Function for writing result in file
+
+
+
+-- Function for writing key in file
