79136258868ff9bae0f3f874f27c1d1bad9e4cfe
tdregmans <tdregmans+github@protonmail.com>
2023-04-18 10:17:06 +0200

    change noOfDigits to index

--- opdracht1/rldecompress.hs
+++ opdracht1/rldecompress.hs
@@ -2,7 +2,7 @@
     lrdecompress.hs
     TINPRO01-8 (Functional programming 2)
     Sep van der Biezen, Thijs Dregmans 
-    Last edited: 2023-04-15
+    Last edited: 2023-04-13
 --}
 
 module Main where
@@ -15,10 +15,10 @@ import Text.Read
 decompress :: [Char] -> [Char]
 decompress x
   | null x = []
-  | noOfDigits == 0 = take 1 x ++ rest
-  | otherwise = replicate (read (take noOfDigits x) :: Int) (x !! noOfDigits) ++ rest
-    where noOfDigits = length (takeWhile isDigit x)
-          rest = decompress (drop (noOfDigits+1) x)
+  | index == 0 = take 1 x ++ rest
+  | otherwise = replicate (read (take index x) :: Int) (x !! index) ++ rest
+    where index = length (takeWhile isDigit x)
+          rest = decompress (drop (index+1) x)
 
 main = do [sourcefile, targetfile] <- getArgs
           filecontent <- readFile sourcefile
