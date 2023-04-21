a59a9813d1d5440499a52d6455aa8e5d80442848
tdregmans <tdregmans+github@protonmail.com>
2023-04-21 22:10:14 +0200

    start opdracht3

---
+++ opdracht3/huffmancompress.hs
@@ -0,0 +1,10 @@
+{--
+    huffmancompress.hs
+    TINPRO01-8 (Functional programming 2)
+    Sep van der Biezen, Thijs Dregmans 
+    Last edited: 2023-04-21
+--}
+
+module Main where
+
+main = do putStrLn "done"
---
+++ opdracht3/huffmandecompress.hs
@@ -0,0 +1,10 @@
+{--
+    huffmandecompress.hs
+    TINPRO01-8 (Functional programming 2)
+    Sep van der Biezen, Thijs Dregmans 
+    Last edited: 2023-04-21
+--}
+
+module Main where
+
+main = do putStrLn "done"
